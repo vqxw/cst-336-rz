83a814b2ce89f862f6e92ccea53f9bb88aeefee3
Raquel Zempoalteca <rzempoalteca5@gmail.com>
2018-02-07 23:48:50 -0800

    committing

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8"/>
+        <title> Raquel Zempoalteca: CS Realted Topic </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <header>
+            <h1> Using CS to Reverse Paralysis </h1>
+        </header>
+        
+        <nav>
+           <hr width = "50%"/>
+           <a href="home.html"><strong> Home </strong></a>
+           <a href="howItsDone.html"> How It's Done </a>
+           <a href="process.html"> Process </a>
+           <a href="result.html"> Result </a>
+        </nav>
+        
+        <main>
+            <figure id = "homePic">
+                <img align = "right" width="350px" height = "350px" src="http://www.browndailyherald.com/wp-content/uploads/2017/03/Catherine-Habgood-Braingate-2.jpeg"/>
+            </figure>
+            
+            <div id = "generalDescription">
+                <p> Over the past years, technology has aided in and solved some of the biggest health problems,</br>for example, paralysis. </p>
+                <p> Paralysis is the loss of ability to move some or all parts of your body. </p>
+                <p> It's caused by illness, disease, or injuries. </p>
+                <p> There isn't a cure for paralysis, however, scientists may have started to figure out a way</br> to restore movement to certain limbs. </p>
+                <p> This website will show you how scientists are able to use technology</br> to help reverse paralysis. </p>
+            </div>
+        </main>
+        
+        <footer>
+            <hr>
+            CST 336. 2018&copy; Zempoalteca <br />
+        </footer>
+        
+    </body>
+
+</html>
